376306264c6f95f9b40ffd34e7744efe7c26324e
neerajp99 <neerajp1999@gmail.com>
2019-09-17 23:31:01 +0530

    Initialise app

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const pasport = require("passport");
+const FacebookStrategy = require("passport-facebook");
+const cors = require("cors");
+const facebookKeys = require("./config/keys").FACEBOOK;
+
+// Creating new facebook-passport strategy
+passport.use()
