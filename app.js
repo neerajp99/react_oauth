33dfaf928400140e17c9cb32be69fdfaa4333573
neerajp99 <neerajp1999@gmail.com>
2019-09-21 02:41:48 +0530

    Modified app.js

--- app.js
+++ app.js
@@ -3,16 +3,50 @@ const passport = require("passport");
 const FacebookStrategy = require("passport-facebook").Strategy;
 const cors = require("cors");
 const keys = require("./config/keys");
+
+// Bring in body-parser middleware
+const bodyParser = require("body-parser");
+// Create an empty object for user details
 let user = {};
+// Bring in mongoose
+const mongoose = require("mongoose");
+// Bring in Facebook User model
+const FacebookUser = require("./models/SocialUser");
 
 // Initialize express app
 const app = express();
+
+// Use bodyParser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// Connecting to mongo databse on mlab
+const db = keys.mongoURI;
+
+mongoose
+  .connect(
+    db,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(res => {
+    console.log("Database connected successfully");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // Initialize passport
 app.use(passport.initialize());
 
 // Use cors for making cross origin requests
-app.use(cors);
+// app.use(cors);
 
 // Creating new facebook-passport strategy
 passport.use(cors());
@@ -28,7 +62,6 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       console.log(JSON.stringify(profile));
       user = { ...profile };
-      done(null, profile);
     }
   )
 );
@@ -44,29 +77,60 @@ app.get("/auth/facebook", passport.authenticate("facebook"));
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
+// app.get(
+//   "/auth/facebook/callback",
+//   passport.authenticate(
+//     ("facebook", { failureRedirect: "/login" }),
+//     (req, res) => {
+//       res.json("Sucess");
+//       res.redirect("/profile");
+//     }
+//   )
+// );
+
 app.get(
-  "auth/facebook/callback",
-  passport.authenticate(
-    ("facebook", { failureRedirect: "/login" }),
-    (req, res) => {
-      res.json("Sucess");
-      res.redirect("/profile");
-    }
-  )
+  "/auth/facebook/callback",
+  passport.authenticate("facebook"),
+  (req, res) => {
+    // res.json("Welcome " + user.displayName);
+    FacebookUser.findOne({
+      facebookId: user.id
+    }).then(user => {
+      if (user) {
+        res.status(401).json("User already exists");
+        done(null, user);
+      } else {
+        const newFacebookUser = new FacebookUser({
+          user: user.displayName,
+          facebookId: user.id
+        })
+          .save()
+          .then(user => {
+            res.json(user);
+            done(null, user);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      }
+    });
+  }
 );
 
 // serializeUser to determine the data of the user object to be stored in the session
 passport.serializeUser((user, done) => {
-  done(null, user);
+  done(null, user.id);
 });
 
+// Take the id and the user from the ID
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
+// Use route
 app.get("/user", (req, res) => {
   res.json({ user });
-  res.send(user);
+  res.send("Welcome ", user.displayName);
 });
 
 // Listening app
